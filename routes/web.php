b731d327ae7e7c273ce96ed026ab151c5a460527
Cimorra2002 <diegocimorra@gmail.com>
2024-11-01 14:37:19 +0100

    change of routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index');
+    return view('index.index');
+});
+
+Route::get('/galeria', function () {
+    return view('photo-gallery.parties');
+});
+
+// Ruta para ver todas las fotos
+Route::get('/todas-las-fotos', function () {
+    return view('todas-las-fotos');
 });
