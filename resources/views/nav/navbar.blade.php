74b3a8c0430934474092e0eb87b22bcebe066831
Cimorra2002 <diegocimorra@gmail.com>
2024-11-01 10:53:37 +0100

    navbar creado

---
+++ resources/views/nav/navbar.blade.php
@@ -0,0 +1,23 @@
+<nav>
+    <!-- Imagen y logo -->
+    <div class="imagen-logo">
+        <!-- Usa la función asset() para acceder a la imagen dentro de la carpeta public -->
+        <a href="{{ url('/') }}"><img src="{{ asset('images/F1logo.svg') }}" alt="Logo"></a>
+        <a href="{{ url('/') }}"><h3>Cimorra Events</h3></a>
+    </div>
+
+    <!-- Checkbox para el menú responsivo -->
+    <input id="menu-toggle" type="checkbox">
+    <label class="menu-button-container" for="menu-toggle">
+        <div class="menu-button"></div>
+    </label>
+
+    <!-- Lista de navegación -->
+    <ul class="lista">
+        <li><a href="{{ url('/') }}">Inicio</a></li>
+        <li><a href="{{ url('/eventos') }}">Eventos</a></li>
+        <li><a href="{{ url('/galeria') }}">Galería</a></li>
+        <li><a href="{{ url('/contacto') }}">Contáctanos</a></li>
+        <li><a href="{{ url('/sobre-nosotros') }}">Sobre Nosotros</a></li>
+    </ul>
+</nav>
