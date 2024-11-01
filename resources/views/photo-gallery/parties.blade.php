aba720c89ad5f2b4645c09984cf296939aefa534
Cimorra2002 <diegocimorra@gmail.com>
2024-11-01 18:07:11 +0100

    add photos and chage styles

--- resources/views/photo-gallery/parties.blade.php
+++ resources/views/photo-gallery/parties.blade.php
@@ -1,26 +1,24 @@
-<!-- resources/views/parties/index.blade.php -->
+<!-- resources/views/photo-gallery/parties.blade.php -->
 @extends('index.index')
 
 @section('title', 'Galería de la Fiesta')
 
 @section('content')
     <div class="gallery-container">
-        <h1>Fiesta de Cumpleaños - 1 de Noviembre de 2024</h1>
+        <h1>Que Resacón en Moogli - 1 de Marzo de 2024</h1>
         <div class="photo-row">
-            <img src="{{ asset('images/foto1.jpg') }}" alt="Foto 1">
-            <img src="{{ asset('images/foto2.jpg') }}" alt="Foto 2">
-            <img src="{{ asset('images/foto3.jpg') }}" alt="Foto 3">
-            <img src="{{ asset('images/foto4.jpg') }}" alt="Foto 4">
+            <img src="{{ asset('images/fiesta2/IMG_11.jpg') }}" alt="Foto 1">
+            <img src="{{ asset('images/fiesta2/IMG_4.jpg') }}" alt="Foto 2">
+            <img src="{{ asset('images/fiesta2/IMG_7.jpg') }}" alt="Foto 3">
         </div>
-        <a href="{{ url('/parties/all') }}" class="view-all-button">Ver todas las fotos</a>
+        <a href="{{ url('/photo-gallery/all-photos') }}" class="view-all-button">Ver todas las fotos</a>
     </div>
     <div class="gallery-container">
-        <h1>Fiesta de Cumpleaños - 1 de Noviembre de 2024</h1>
+        <h1>Que Resacón en Bloody - 21 de diciembre de 2023</h1>
         <div class="photo-row">
-            <img src="{{ asset('images/foto1.jpg') }}" alt="Foto 1">
-            <img src="{{ asset('images/foto2.jpg') }}" alt="Foto 2">
-            <img src="{{ asset('images/foto3.jpg') }}" alt="Foto 3">
-            <img src="{{ asset('images/foto4.jpg') }}" alt="Foto 4">
+            <img src="{{ asset('images/fiesta1/IMG_16.jpg') }}" alt="Foto 1">
+            <img src="{{ asset('images/fiesta1/IMG_18.jpg') }}" alt="Foto 2">
+            <img src="{{ asset('images/fiesta1/IMG_17.jpg') }}" alt="Foto 3">
         </div>
         <a href="{{ url('/parties/all') }}" class="view-all-button">Ver todas las fotos</a>
     </div>
