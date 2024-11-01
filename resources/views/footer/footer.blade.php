f4cb44eb8acd375ca0f05a57de08ce2bd14158a1
Cimorra2002 <diegocimorra@gmail.com>
2024-11-01 18:06:43 +0100

    footer

---
+++ resources/views/footer/footer.blade.php
@@ -0,0 +1,16 @@
+<!-- Pie de página -->
+<footer>
+    <div class="info">
+        <br>
+        <p>Siguenos en:</p>
+        <p>Que Resacón: <a class="text-secondary" href="https://www.instagram.com/queresacon/" target="_blank">Instagram</a></p>
+        <p>Que Resacón ZGZ: <a class="text-secondary" href="https://www.instagram.com/queresaconzgz/" target="_blank">Instagram</a></p>
+    </div>
+    <div class="info">
+        <p>Contactanos:</p>
+        <p>info@queresacon.com</p>
+    </div>
+    <div class="copyright">
+        © 2024 Copyright
+    </div>
+</footer>
